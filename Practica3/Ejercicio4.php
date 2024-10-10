e9c9da93cb9ebfc13ce42eedfcbcab2630625c5c
Daniel Ramos <danidrf06@gmail.com>
2024-10-10 13:39:05 +0200

    Jueves

--- Practica3/Ejercicio4.php
+++ Practica3/Ejercicio4.php
@@ -1,13 +1,18 @@
 <?php 
-
-$array=[]; 
-$limite=5; 
-for ($i=0;$i<10:$i++){ 
-    if (count($array)<$limite){
-        
+function array_limitado($array,$limite){
+    $array_limitado= []; 
+    for($i=0; $i<count($array);$i++){ 
+        if($i<$limite){ 
+            $array_limitado[]= $array[$i]; 
+        }else{ 
+            break;
+        }
     }
-}
+    return $array_limitado;
+ }
 
-for
-
+ $array_o=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+ $limite=5; 
+ $array_limitado=array_limitado($array_o,$limite); 
+print_r($array_limitado);
 ?>
---
+++ Pruebas_O/test.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'personas.php'; 
+//Creamos el objeto.
+$persona = new Persona();
+//Seteamos las propiedades.
+$persona->nombre = 'Daniel';
+$persona->apellido = 'Ramos';
+$persona->edad = 20;
+//Mostramos el resultado de las propiedades.
+echo 'Nombre: ' . $persona->nombre . '<br />';
+echo 'Apellido: ' . $persona->apellido . '<br />';
+echo 'Edad: ' . $persona->edad . '<br />';
+?>

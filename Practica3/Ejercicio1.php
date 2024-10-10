1a199b8ded98d2883552c5e57ad6f8c02e62fa96
Daniel Ramos <danidrf06@gmail.com>
2024-10-10 12:05:54 +0200

    Jueves

--- Practica3/Ejercicio1.php
+++ Practica3/Ejercicio1.php
@@ -1,7 +1,6 @@
 <!-- Crea una función para resolver la ecuación de segundo grado. Esta función recibe
 los coeficientes de la ecuación y devuelve un array con las soluciones. Si no hay
-soluciones reales, devuelve FALSE.
- -->
+soluciones reales, devuelve FALSE. -->
  <?php
 function ecuacion_segundo_grado($a, $b, $c) {
     if ($a == 0) {

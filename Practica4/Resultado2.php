eb9fd3c538f4aa60cfe20e3e6a07f7a73f9b80ac
Daniel Ramos <danidrf06@gmail.com>
2024-10-16 14:22:33 +0200

    Miercoles

--- Practica4/Resultado2.php
+++ Practica4/Resultado2.php
@@ -1,4 +1,34 @@
 <?php
+include 'Ejercicio2.php';
 
+class Ejecutor {
+    public function ejecutar() {
+        // Creación de un objeto de la clase DosRuedas (moto)
+        $moto = new DosRuedas("Rojo", 100, 125);
+        echo "Moto creada.<br>";
+        $moto->circula();
+        $moto->poner_gasolina(5);
 
+        echo "<br>";
+
+        // Creación de un objeto de la clase CuatroRuedas (coche)
+        $coche = new CuatroRuedas("Azul", 1500, 4);
+        echo "Coche creado.<br>";
+        $coche->circula();
+        $coche->repintar("Negro");
+        $coche->añadir_persona(70);
+
+        echo "<br>";
+
+        // Creación de un objeto de la clase Camion
+        $camion = new Camion("Blanco", 3000, 2, 8);
+        echo "Camión creado.<br>";
+        $camion->añadir_persona(80);
+        $camion->añadir_remolque(4.5);
+    }
+}
+
+// Ejecución del ejemplo
+$ejecutor = new Ejecutor();
+$ejecutor->ejecutar();
 ?>

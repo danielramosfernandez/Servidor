649b1172a1eb3e49867b3969fb07f7ae6e107bf7
Daniel Ramos <danidrf06@gmail.com>
2024-11-04 14:25:51 +0100

    l

---
+++ Tema_4/Practica2/Simon.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Círculo en HTML</title>
+    <style>
+        .circulo {
+            display: inline-block;
+            width: 100px;          
+            height: 100px;        
+            background-color:red ; 
+            border-radius: 50%;  
+               
+            margin: 20px;          
+        }
+    </style>
+</head>
+<body>
+    <div class="circulo"></div>
+    <div class="circulo"></div>
+    <div class="circulo"></div>
+    <div class="circulo"></div> 
+    <script>
+    <?php
+    function colores(){ 
+        $colores = ["#FF0000","#0000FF", "#FFFF00","#0FF000"]; 
+        
+    }
+    ?>
+    </script>
+</body>
+</html>

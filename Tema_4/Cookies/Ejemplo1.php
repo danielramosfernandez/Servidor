3d4620612b10217e271b30a015f2a2aff56336f9
Daniel Ramos <danidrf06@gmail.com>
2024-11-18 14:22:36 +0100

    Lunes

---
+++ Tema_4/Cookies/Ejemplo1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COOKIES</title> 
+        <style>
+    
+            .container{ 
+                width: 100px;
+                height: 75px; 
+                border: 2px;
+                border: radius 1px;
+            }
+        </style>
+    </head>
+    <body>
+        
+    </body>
+</html>

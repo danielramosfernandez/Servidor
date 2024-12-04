8c8c6778884e449ddb72594004c2d8d71e84cde7
Daniel Ramos <danidrf06@gmail.com>
2024-12-04 14:22:09 +0100

    Miercoles

--- CARTAS/Login.php
+++ CARTAS/Login.php
@@ -2,5 +2,12 @@
  $hn = 'localhost';
  $db = 'cartas';
  $un = 'root';
- $pw = '';
+ $pw = ''; 
+ // Crear conexión
+ $conn = new mysqli($hn,$un,$pw,$db,3307); 
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
 ?> 
